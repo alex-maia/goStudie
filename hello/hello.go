2b38e4aa9d957e615d916d89e7e05d57c56d9132
alex-maia <alexandre.maia1@sapo.pt>
2025-08-29 20:11:38 +0100

    greetings and hello exercise

---
+++ hello/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"example.com/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Alex")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
