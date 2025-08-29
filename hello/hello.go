077ceff1349a740fc96f7fdb3cbd76679d239873
alex-maia <alexandre.maia1@sapo.pt>
2025-08-30 00:12:07 +0100

    Teste 3 lazygit

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,7 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Alex")
+	message, err := greetings.Hello("Banana")
 
 	if err != nil {
 		log.Fatal(err)
